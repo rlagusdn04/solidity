790dd58113ed09e55ca26a7220d5d10d30d6f562
rlagusdn04 <68765352+rlagusdn04@users.noreply.github.com>
2025-03-31 16:12:08 +0900

    first commit

---
+++ test.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT 
+
+
+
+contract Hello {
+    string public greeting = "Hello, World!"; // Public variable to store the greeting message
+    uint256 public number = 42; // Public variable to store a number
+    address public owner; // Public variable to store the owner's address
+    
+}
